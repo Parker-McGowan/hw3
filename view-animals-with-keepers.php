af6febc12570129c865e09914d506b24f8a3cc3e
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-04 11:13:17 -0500

    Update view-animals-with-keepers.php

--- view-animals-with-keepers.php
+++ view-animals-with-keepers.php
@@ -7,13 +7,17 @@ while ($animal = $aniamls->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $animal['animal_name'];?></h5>
       <p class="card-text">
+        <ul class="list-group">
   <?php
   $keepers = selectKeeperbyAnimal($animal['animal_id']);
   while ($keeper = $keeper->fetch_assoc()) {
     ?>
+      <li class="list-group-item"><?php echo $animal['habitat_name']; ?> - <?php echo $animal['habitat_type']; ?></li>
+
       <?php
   }
   ?>
+    </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Species: <?php echo $animal['species_name']; ?></small></p>
     </div>
