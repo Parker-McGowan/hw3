8ddd40d62724a9d0001be3db7f2275ebb574a41b
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-04 10:37:15 -0500

    Create animals-with-keepers.php

---
+++ animals-with-keepers.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-animals-with-keepers.php";
+
+$pageTitle = "Animals with keepers";
+include "view-header.php";
+$animals = selectAnimals();
+include "view-animals-with-keepers.php";
+include "view-footer.php";
+?>
