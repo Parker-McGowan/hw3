780ec56e411365eeb2bb11e599afa3e269a56225
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-04 01:31:35 -0500

    Update view-animals.php

--- view-animals.php
+++ view-animals.php
@@ -6,6 +6,7 @@
     <th>ID</th>
     <th>Name</th>
     <th>Species</th>
+    <th></th>
   </tr>
   <thead>
     <tbody>
@@ -16,6 +17,7 @@ while ($animal = $aniamls->fetch_assoc()) {
     <td><?php echo $animal['animal_id']; ?><</td>
     <td><?php echo $animal['animal_name']; ?><</td>
     <td><?php echo $animal['species_name']; ?><</td>
+      <td><a href="habitat-by-animal.php?id=<?php echo $animal['animal_id']; ?>">Courses</a></td>
   </tr>
   <?php
 }
