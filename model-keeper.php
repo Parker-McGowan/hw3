c53629a26c8f15c0551b0603585ce947bfabc262
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-04 01:22:49 -0500

    Create model-keeper.php

---
+++ model-keeper.php
@@ -0,0 +1,15 @@
+<?php
+function selectKeepers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT keeper_id, first_name, last_name, contact FROM `keeper`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
