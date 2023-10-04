5d97935e7d883ef4c9d582caaadfea7eceb32531
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-04 10:44:57 -0500

    Update model-animals-with-keepers.php

--- model-animals-with-keepers.php
+++ model-animals-with-keepers.php
@@ -12,4 +12,19 @@ function selectAnimals() {
         throw $e;
     }
 }
+
+function selectHabitatsByAnimal($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT h.habitat_id, habitat_name, habitat_type FROM `habitat` h join animal a on a.habitat_id = h.habitat_id WHERE a.animal_id = ?");
+        $stmt->blind_param("i", $iid);
+      $stmt->execute();
+      $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
