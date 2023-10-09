144f18d6f2ae9adb64ffa4dd2ec188bad0d92bc6
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-09 13:48:55 -0500

    Update keeper.php

--- keeper.php
+++ keeper.php
@@ -4,7 +4,7 @@ require_once("model-keeper.php";
 
 $pageTitle = "Keeper";
 include "view-header.php";
-$keeper = selectKeeper();
+$keeper = selectKeepers();
 include "view-keeper.php";
 include "view-footer.php";
 ?>
