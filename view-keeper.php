0d00080929b4a79b972cf827bee7305138ce28e9
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-18 13:47:01 -0500

    Update view-keeper.php

--- view-keeper.php
+++ view-keeper.php
@@ -18,6 +18,7 @@ while ($keeper = $keepers->fetch_assoc()) {
     <td><?php echo $keeper['first_name']; ?></td>
       <td><?php echo $keeper['last_name']; ?></td>
     <td><?php echo $keeper['contact']; ?></td>
+    <td><a href="animals-with-keepers.php">Animals</a></td>
   </tr>
   <?php
 }
