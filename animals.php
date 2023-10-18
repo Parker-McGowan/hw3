3e7ed002e63612aa584aa1942a5517eb46516bca
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-18 12:26:34 -0500

    Update animals.php

--- animals.php
+++ animals.php
@@ -4,7 +4,7 @@ require_once("model-animals.php");
 
 $pageTitle = "Animals";
 include "view-header.php";
-$animals = selectAnimals();
+$animals = selectAnimal();
 include "view-animals.php";
 include "view-footer.php";
 ?>
