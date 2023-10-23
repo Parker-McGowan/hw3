9a330a1883d8b1c2da6efe4765e5d20b999dab2a
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 13:22:19 -0500

    Update model-habitat-by-animal.php

--- model-habitat-by-animal.php
+++ model-habitat-by-animal.php
@@ -1,9 +1,9 @@
 <?php
-function selectHabitatbyAnimal($id) {
+function selectHabitatbyAnimal($aid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT h.habitat_id, habitat_name, habitat_type FROM `habitat` h join animal a on a.habitat_id = h.habitat_id WHERE a.animal_id = ?");
-        $stmt->blind_param("i", $id);
+        $stmt->blind_param("i", $aid);
       $stmt->execute();
       $result = $stmt->get_result();
         $conn->close();
