c35ad467d77dad1e4a10317773d1dab9ef1b5156
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 11:07:45 -0500

    Update and rename habitats-by-animal.php to habitatt-by-animal.php

---
+++ habitatt-by-animal.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-habitat-by-animal.php";
+
+$pageTitle = "Habitat by Aniaml";
+include "view-header.php";
+$habitat = selectHabitatbyAniaml($_POST['aid']);
+include "view-habitat-by-aniaml.php";
+include "view-footer.php";
+?>
