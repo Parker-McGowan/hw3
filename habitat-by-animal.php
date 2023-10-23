0bab1b69ab1d8d60bb0aae2ae4ed453b84b4c62c
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 13:21:25 -0500

    Update habitat-by-animal.php

--- habitat-by-animal.php
+++ habitat-by-animal.php
@@ -4,7 +4,7 @@ require_once("model-habitat-by-animal.php");
 
 $pageTitle = "Habitat by Animal";
 include "view-header.php";
-$habitat = selectHabitatbyAnimal($_POST['aid']);
+$habitat = selectHabitatbyAnimal($_POST['id']);
 include "view-habitat-by-animal.php";
 include "view-footer.php";
 ?>
