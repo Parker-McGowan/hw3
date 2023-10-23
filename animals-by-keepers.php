f86b1ee65ecb5d650961addbefc45c4c27ca707e
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 10:18:39 -0500

    Update animals-by-keepers.php

--- animals-by-keepers.php
+++ animals-by-keepers.php
@@ -4,7 +4,7 @@ require_once("model-animals-by-keepers.php");
 
 $pageTitle = "Animal by Keeper";
 include "view-header.php";
-$animals = selectAniamlByKepper($_GET['id');
+$animals = selectAniamlByKepper($_GET['id']);
 include "view-animal-by-keeper.php";
 include "view-footer.php";
 ?>
