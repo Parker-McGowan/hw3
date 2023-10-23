40507cae3e31b1bc26a284e2ed1dbc1f6ac760ab
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 09:52:14 -0500

    Update model-animal-by-keeper.php

--- model-animal-by-keeper.php
+++ model-animal-by-keeper.php
@@ -2,7 +2,7 @@
 function selectAnimalByKeeper($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT a.animal_id, animal_name, species_name,keeper_id FROM `animal` a where a.keeper_id>=1");
+        $stmt = $conn->prepare("SELECT a.animal_id, animal_name, species_name,keeper_id FROM `animal` a where a.keeper_id=?");
         $stmt->blind_param("i", $aid);
       $stmt->execute();
       $result = $stmt->get_result();
