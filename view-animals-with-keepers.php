4ac3b3eff4c18758212624e1cae356d742a07081
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-23 14:22:32 -0500

    Update view-animals-with-keepers.php

--- view-animals-with-keepers.php
+++ view-animals-with-keepers.php
@@ -1,7 +1,7 @@
 <h1>Animals with keepers</h1>
 <div class="card-group">  
 <?php
-while ($animal = $aniamls->fetch_assoc()) {
+while ($animal = $animals->fetch_assoc()) {
   ?>
   <div class="card">
     <div class="card-body">
