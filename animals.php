e6b9a84fec61dedcbde5feaeeaa28e09dfe90db9
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 01:54:07 -0500

    Update animals.php

--- animals.php
+++ animals.php
@@ -14,6 +14,13 @@ switch ($_POST['actionType']) {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
   break;
+   case "Edit":
+    if (updateAnimal($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'], $_POST['aid'])) {
+     echo '<div class="alert alert-success" role="alert">Animal edited.</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+  break;
    case "Delete":
     if (deleteAnimal($_POST['aid'])) {
      echo '<div class="alert alert-success" role="alert">Animal deleted.</div>';
