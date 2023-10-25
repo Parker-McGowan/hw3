8fcf2f2beabe6f106cb0133b43ea39148939dc21
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-24 21:37:04 -0500

    Update animals.php

--- animals.php
+++ animals.php
@@ -4,6 +4,14 @@ require_once("model-animals.php");
 
 $pageTitle = "Animals";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+switch ($_POST['actionType']) { 
+  case "Add":
+    insertAnimal($_POST['aName'], $_POST['aSpecies']);
+  break;
+  }
+}
 $animals = selectAnimal();
 include "view-animals.php";
 include "view-footer.php";
