16884a0c1b035f36836388d89a4c362ec1ebc1f0
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 10:25:16 -0500

    Update animals-with-keepers.php

--- animals-with-keepers.php
+++ animals-with-keepers.php
@@ -5,6 +5,7 @@ require_once("model-animals-with-keepers.php");
 $pageTitle = "Animals with Keepers";
 include "view-header.php";
 
+
 $animals = selectAnimals();
 include "view-animals-with-keepers.php";
 include "view-footer.php";
