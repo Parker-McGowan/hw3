33ccead7b0b13121f67091a47f2e57f1a50c39a2
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 08:45:31 -0500

    Update view-keeper.php

--- view-keeper.php
+++ view-keeper.php
@@ -1,4 +1,13 @@
+<div class = "row">
+  <div class ="col">
 <h1>Keepers</h1>
+  </div>
+  <div class="col-auto">
+    <?php
+include "view-keeper-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
 <thead>
