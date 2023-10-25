f1d8f9c5dc7e9ef55c3eaefb0f15f0d782949f99
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-24 22:20:09 -0500

    Update animals.php

--- animals.php
+++ animals.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
 switch ($_POST['actionType']) { 
   case "Add":
-    insertAnimal($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid']);
+    if (insertAnimal($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'])) {
+     echo '<div class="alert alert-success" role="alert">Animal added.</div>"';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+    }
   break;
   }
 }
