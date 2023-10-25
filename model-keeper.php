0eca74cc913ddfc313a0d07c9d54242ec0748974
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 08:37:06 -0500

    Update model-keeper.php

--- model-keeper.php
+++ model-keeper.php
@@ -26,5 +26,32 @@ function insertKeeper($kFirst, $kLast, $kContact) {
         throw $e;
     }
 }
+function deleteKeeper($kid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from keeper where keeper_id=?");
+        $stmt->bind_param("i", $kid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateKeeper($kFirst, $kLast, $kContact, $kid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `keeper` set `first_name` = ?, `last_name` = ?, `contact` = ?, `keeper_id` = ? where keeper_id = ?");
+        $stmt->bind_param("sssi", $kFirst, $kLast, $kContact, $kid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
