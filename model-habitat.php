88d14d951a93b3ba438c47858cbd29f3ee80f141
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 14:01:34 -0500

    Update model-habitat.php

--- model-habitat.php
+++ model-habitat.php
@@ -12,4 +12,45 @@ function selectHabitats() {
         throw $e;
     }
 }
+function deleteHabitat($hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from habitat where habitat_id=?");
+        $stmt->bind_param("i", $hid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateAnimal($hName, $hType, $hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `habitat` set `habitat_name` = ?, `habitat_type` = ?, `habitat_id` = ?, `keeper_id` = ? where animal_id = ?");
+        $stmt->bind_param("ssiii", $aName, $aSpecies, $aHabitatid, $aKeeperid, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertAnimal($aName, $aSpecies, $aHabitatid, $aKeeperid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `animal` (`animal_name`, `species_name`, `habitat_id`, `keeper_id`) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssii", $aName, $aSpecies, $aHabitatid, $aKeeperid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
