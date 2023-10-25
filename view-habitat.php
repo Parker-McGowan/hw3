489ed391fc4788c1dc63da4df05cf31b8e7dcf76
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 16:49:42 -0500

    Update view-habitat.php

--- view-habitat.php
+++ view-habitat.php
@@ -1,4 +1,13 @@
+<div class ="row">
+  <div class = "col">
 <h1>Habitat</h1>
+  </div>
+<div class="col-auto">
+  <?php
+include "view-animals-newform.php";
+?>
+</div>
+</div>
 <div class="table-responsive">
   <table class="table">
 <thead>
@@ -18,9 +27,14 @@ while ($habitat = $habitats->fetch_assoc()) {
     <td><?php echo $animal['habitat_name']; ?><</td>
     <td><?php echo $animal['habitat_type']; ?><</td>
       <td>
+        <?php
+  include "view-animals-editform.php";
+  ?>
+      </td>
+      <td>
         <form method="post" action="habitat-by-animal.php" >
-          <input type = "hidden" name = "cid" value = "<?php echo $animal['habitat_id']; ?>">
-  <button type="submit" class="btn btn-primary">Habitats</button>
+          <input type = "hidden" name = "hid" value = "<?php echo $habitat['habitat_id']; ?>">
+  <button type="submit" class="btn btn-primary">Habitat</button>
 </form>
       </td>
   </tr>
