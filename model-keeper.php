19d74b7dc2781a6cce9eb1df7810afd8c67922e3
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 08:24:48 -0500

    Update model-keeper.php

--- model-keeper.php
+++ model-keeper.php
@@ -12,4 +12,18 @@ function selectKeeper() {
         throw $e;
     }
 }
+
+function insertKeeper($kFirst, $kLast, kContact) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `keeper` (`first_name`, `last_name`, `contact`) VALUES ('?', '?', '?');");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
