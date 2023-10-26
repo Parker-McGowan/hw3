57a9d862b454f8c03bf380958a1eb34c0b3f3eb3
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-10-25 23:07:12 -0500

    Update habitat-by-animal.php

--- habitat-by-animal.php
+++ habitat-by-animal.php
@@ -4,6 +4,33 @@ require_once("model-habitat-by-animal.php");
 
 $pageTitle = "Habitat by Animal";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+switch ($_POST['actionType']) { 
+  case "Add":
+    if (insertHabitatbyAnimal($_POST['hName'], $_POST['hType'])) {
+     echo '<div class="alert alert-success" role="alert">Habitat added.</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+  break;
+   case "Edit":
+    if (updateHabitatbyAnimal($_POST['hName'], $_POST['hType'], $_POST['hid'])) {
+     echo '<div class="alert alert-success" role="alert">Habitat edited.</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+  break;
+   case "Delete":
+    if (deleteHabitatbyAnimal($_POST['hid'])) {
+     echo '<div class="alert alert-success" role="alert">Habitat deleted.</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+  break;
+  }
+}
+
 $habitats = selectHabitatbyAnimal($_POST['hid']);
 include "view-habitat-by-animal.php";
 include "view-footer.php";
