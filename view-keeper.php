7d6a57e9b3a333d135eb9d4c1b1cbda243c42035
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-11-15 09:26:56 -0600

    Update view-keeper.php

--- view-keeper.php
+++ view-keeper.php
@@ -9,6 +9,18 @@
     <th>Contact</th>
     <th></th>
   </tr>
+data = {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+};
   <thead>
     <tbody>
       <?php
