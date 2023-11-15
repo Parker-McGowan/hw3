a7ae9bb68cf2cdf9a3bccff46bdac9b24272aa4d
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-11-14 22:43:14 -0600

    Create animals-chart.php

---
+++ animals-chart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-animals-chart-db.php");
+
+$pageTitle = "Animals Chart";
+include "view-header.php";
+$animals = selectAnimal();
+include "view-animals-chart.php";
+include "view-footer.php";
+?>
