078a6f5af608185c8cf72c139e715bd8860a847c
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-12 20:29:32 -0600

    Update animals-with-keepers.php

--- animals-with-keepers.php
+++ animals-with-keepers.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
 switch ($_POST['actionType']) { 
   case "Add":
-    if (insertAnimalswithkeepers($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'])) {
+    if (insertAnimal($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'])) {
      echo '<div class="alert alert-success" role="alert">Animal added.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
   break;
    case "Edit":
-    if (updateAnimalswithkeepers($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'], $_POST['aid'])) {
+    if (updateAnimal($_POST['aName'], $_POST['aSpecies'], $_POST['aHabitatid'], $_POST['aKeeperid'], $_POST['aid'])) {
      echo '<div class="alert alert-success" role="alert">Animal edited.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
   break;
    case "Delete":
-    if (deleteAnimalswithkeepers($_POST['aid'])) {
+    if (deleteAnimal($_POST['aid'])) {
      echo '<div class="alert alert-success" role="alert">Animal deleted.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
@@ -32,7 +32,7 @@ switch ($_POST['actionType']) {
 }
 
 
-$animals = selectAnimalswithkeepers();
+$animals = selectAnimal();
 include "view-animals-with-keepers.php";
 include "view-footer.php";
 ?>
